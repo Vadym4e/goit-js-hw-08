262aabbc381f87728ac5a31f25dd6a21b22aa9af
Vadym4e <vadym.o92@gmail.com>
2023-03-05 12:48:08 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(evt => {
+    localStorage.setItem('lastWatchedTime', evt.seconds);
+  }, 1000)
+);
+player.setCurrentTime(localStorage.getItem('lastWatchedTime') || 0);
